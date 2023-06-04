aeee080db75a09bfbc6100b87e1c7c9f95cb8ef6
Kazuki Burrows <kazuiscool@gmail.com>
2023-06-04 19:45:44 +1200

    first commit

---
+++ CardFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethodDesignPatternInCSharp
+{
+    /// <summary>  
+    /// The 'Creator' Abstract Class  
+    /// </summary>  
+    abstract class CardFactory
+    {
+        public abstract CreditCard GetCreditCard();
+    }
+}
